f02358c8ca602a9df929ad002c08a3cc2720df28
Goober118 <140124210+Goober118@users.noreply.github.com>
2025-06-02 10:37:02 +1000

    Create main.py
    Begun working on the roulette wheel algorithm

---
+++ main.py
@@ -0,0 +1,13 @@
+import math
+import random
+
+def main():
+    num = random.randint(1,36)
+    bet_choice1 = input(int("Which odds would you like to bet on?\n1. Colours\n2. Numbers\nInput:"))
+    if bet_choice1 == 1:
+        bet_choice1a = input(int("Which colour would you like to bet on?\n1. Red\n2. Black\nInput:"))
+        for i in range(36):
+            if 0 < 
+    
+
+main()
