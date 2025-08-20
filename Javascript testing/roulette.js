3bfb64020085b5b4ff5b218104f5b83fc3bc6228
Goober118 <140124210+Goober118@users.noreply.github.com>
2025-08-20 13:44:39 +1000

    Added a spin mechanic that randomises the result number, then animates the roulette wheel spinning to achieve that number. Displays the correct result by using an array of wheelNumbers. Also straightened the numbers by using block position rather than flex position in CSS.

--- Javascript testing/roulette.js
+++ Javascript testing/roulette.js
@@ -1,16 +1,39 @@
 
 const wheel = document.getElementById("outer-bands");
+const result = document.getElementById("result");
 const offset = 360 / 37 / 2;
+// Array of wheel numbers offset by 1 counterclockwise to match the colour bands
 const wheelNumbers = [
-    0, 32, 15, 19, 4, 21, 2, 25, 17, 34, 6, 27, 13, 36, 11, 30,
+    26, 0, 32, 15, 19, 4, 21, 2, 25, 17, 34, 6, 27, 13, 36, 11, 30,
     8, 23, 10, 5, 24, 16, 33, 1, 20, 14, 31, 9, 22, 18, 29, 7,
-    28, 12, 35, 3, 26
+    28, 12, 35, 3
 ]
 const numbersContainer = document.getElementById("numbers");
+
+
 const anglePerNumber = 360 / 37;
+
 wheelNumbers.forEach((num, i) => {
-    const span = document.createElement("span");
-    span.textContent = num;
-    span.style.transform = `rotate(${i * anglePerNumber}deg) translateY(-140px)`;
+
+    const span = document.createElement("span"); // Create one span element for each number
+    span.textContent = num; // Insert the number into the corresponding span position
+    span.style.transform = `rotate(${i * anglePerNumber}deg) translateY(-137px)`; // position each span element by multiplying the band angle size by the index number, then translate it outwards to create a 140px radius
     numbersContainer.appendChild(span);
 });
+
+let spinCount = 0;
+
+function spin() {
+    const spinNumber = Math.floor(Math.random() * 37);
+    const anglePerNumber = 360 / 37;
+
+    spinCount++;
+    let targetRotation = (360 * 5 * spinCount) + (360 - spinNumber * anglePerNumber) + offset; 
+
+    wheel.style.transform = `rotate(${targetRotation}deg)`;
+
+    setTimeout(() => {
+        result.textContent = "Result: " + wheelNumbers[spinNumber];
+    }, 4000);
+}
+
