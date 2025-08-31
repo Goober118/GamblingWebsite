ce4463162801b70124a829f37d0c3eec442cd9ed
Goober118 <140124210+Goober118@users.noreply.github.com>
2025-09-01 07:45:17 +1000

    Created a login page that links to the "logout" button on the roulette page. Created some CSS and JS code to stylise it, allowing users to switch between logging in and signing up.

---
+++ Javascript testing/login.js
@@ -0,0 +1,18 @@
+const loginBtn = document.getElementById('login-btn');
+const signupBtn = document.getElementById('signup-btn');
+const loginForm = document.getElementById('login-form');
+const signupForm = document.getElementById('signup-form');
+
+loginBtn.addEventListener('click', () => {
+    loginBtn.classList.add('active');
+    signupBtn.classList.remove('active');
+    loginForm.classList.add('active');
+    signupForm.classList.remove('active');
+});
+
+signupBtn.addEventListener('click', () => {
+    signupBtn.classList.add('active');
+    loginBtn.classList.remove('active');
+    signupForm.classList.add('active');
+    loginForm.classList.remove('active');
+});
