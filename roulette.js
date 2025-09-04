34721660af142c238fd49d46ff6ff6c7bc0426cb
Goober118 <140124210+Goober118@users.noreply.github.com>
2025-09-04 12:35:50 +1000

    Sacrificial commit

--- roulette.js
+++ roulette.js
@@ -47,15 +47,15 @@ function main() {
     document.querySelectorAll(".two-chip, .five-chip, .ten-chip, .twenty-chip, .fifty-chip").forEach(chip => {
         chip.addEventListener("dragstart", e => {
             // store chip class and value upon dragstart
-            e.dataTransfer.setData("text/plain", chip.dataset.value);
+            e.dataTransfer.setData("value", chip.dataset.value);
             e.dataTransfer.setData("chipClass", chip.className);
         });
     });
 
     // drop chips
     document.querySelectorAll(".bet-option, .bet-button, .side-button, .extra-button, .zero-button").forEach(spot => {
-        spot.addEventListener("dragover", chip => {
-            chip.preventDefault(); // allow the chip to be dropped
+        spot.addEventListener("dragover", e => {
+            e.preventDefault(); // allow the chip to be dropped
         });
 
         // detect drop, and call the handleDrop function
@@ -115,8 +115,7 @@ function handleDrop(e, spot) {
     if (oldChip) oldChip.remove();
 
     // retrieve chip class and value
-    const amount = parseInt(e.dataTransfer.getData("text/plain"), 10);
-    console.log("Dropped chip amount:", amount);
+    const amount = parseInt(e.dataTransfer.getData("value"), 10);
     const chipClass = e.dataTransfer.getData("chipClass").split(" ")[0];
 
     // determine bet type
@@ -171,7 +170,7 @@ function handleDrop(e, spot) {
         value = parseInt(spot.textContent, 10);
     }
 
-    console.log("Wallet amount:", walletAmount);
+    
     if (walletAmount >= amount) { // only allow a bet to be placed if the player's wallet has enough funds
         
         // deduct the bet amount from the wallet and update the display
@@ -189,14 +188,14 @@ function handleDrop(e, spot) {
         chip.style.cursor = "pointer";
 
         // functionality to remove chips
-        chip.addEventListener("click", function(chip) {
+        chip.addEventListener("click", function(e) {
             if (isSpinning) {
                 message = "The wheel is currently spinning"
                 showErrorPopup(message);
                 return;
             }
             singleChip.play();
-            chip.stopPropagation();
+            e.stopPropagation();
             chip.remove();
 
             // refund the chip amount back to the wallet
